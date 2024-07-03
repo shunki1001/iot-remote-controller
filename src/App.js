54c1ef7187bd17a40d29bc7d055e6357bba1cafb
Shunki <tajimura0505@gmail.com>
2024-07-03 20:59:38 +0900

    #1 ボタン追加

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React from "react";
 import { Button, Grid, Container, Typography } from "@mui/material";
 import axios from "axios";
 
-const host_domain = "http://127.0.0.1:5001";
+const host_domain =
+  "https://asia-northeast1-doctor-ilcsi.cloudfunctions.net/python-backend-function";
 
 const buttons = [
-  { label: "Television On", endpoint: "/tv/on" },
-  { label: "Turn Off Light", endpoint: "/api/light/off" },
-  { label: "Lock Door", endpoint: "/api/door/lock" },
-  { label: "Unlock Door", endpoint: "/api/door/unlock" },
+  { label: "/tv/on", endpoint: "/tv/on" },
+  { label: "/living-ac/on", endpoint: "/living-ac/on" },
+  { label: "/living-ac/off", endpoint: "/living-ac/off" },
+  { label: "/desk-ac/on", endpoint: "/desk-ac/on" },
+  { label: "/desk-ac/off", endpoint: "/desk-ac/off" },
+  { label: "/living-light/on", endpoint: "/living-light/on" },
+  { label: "/living-light/off", endpoint: "/living-light/off" },
+  { label: "/living-light/dinner", endpoint: "/living-light/dinner" },
+  { label: "/living-light/lunch", endpoint: "/living-light/lunch" },
+  { label: "/desk-light/on", endpoint: "/desk-light/on" },
+  { label: "/desk-light/off", endpoint: "/desk-light/off" },
+  { label: "/desk-light/brighten", endpoint: "/desk-light/brighten" },
+  { label: "/desk-light/darken", endpoint: "/desk-light/darken" },
 ];
 
 const handleButtonClick = (endpoint) => {
