c26110bf342ff1c3f3c08c94d0fd12ed3973f2cd
Shunki <tajimura0505@gmail.com>
2024-08-04 20:35:06 +0900

    温湿度を追加　ボタンをまとめる

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Button,
   Grid,
@@ -7,6 +7,8 @@ import {
   Backdrop,
   CircularProgress,
   Snackbar,
+  CardContent,
+  Card,
 } from "@mui/material";
 import axios from "axios";
 
@@ -26,36 +28,14 @@ const buttons = [
   { label: "書斎エアコンON", endpoint: "/desk-ac/on", category: "primary" },
   { label: "書斎エアコンOFF", endpoint: "/desk-ac/off", category: "primary" },
   {
-    label: "リビング電気ON",
-    endpoint: "/living-light/on",
+    label: "電気ON",
+    endpoint: "/light/on",
     category: "error",
   },
   {
-    label: "リビング電気OFF",
-    endpoint: "/living-light/off",
+    label: "電気OFF",
+    endpoint: "/light/off",
     category: "error",
-  },
-  {
-    label: "リビング電気明るく",
-    endpoint: "/living-light/dinner",
-    category: "error",
-  },
-  {
-    label: "リビング電気暗く",
-    endpoint: "/living-light/lunch",
-    category: "error",
-  },
-  { label: "書斎電気ON", endpoint: "/desk-light/on", category: "primary" },
-  { label: "書斎電気OFF", endpoint: "/desk-light/off", category: "primary" },
-  {
-    label: "書斎電気明るく",
-    endpoint: "/desk-light/brighten",
-    category: "primary",
-  },
-  {
-    label: "書斎電気暗く",
-    endpoint: "/desk-light/darken",
-    category: "primary",
   },
   { label: "TVの電源", endpoint: "/tv/on", category: "primary" },
 ];
@@ -64,6 +44,47 @@ const App = () => {
   const [open, setOpen] = useState(false);
   const [bdOpen, setBdOpen] = useState(false);
   const [response, setResponse] = useState("");
+  const [thermodata, setThermodata] = useState([]);
+  const [fetchTime, setFetchTime] = useState(new Date());
+
+  useEffect(() => {
+    // APIを定期的に呼び出す関数
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(host_domain + "/thermo-sensor");
+        setThermodata(response.data["status"]);
+        setFetchTime(new Date());
+      } catch (error) {
+        setThermodata([
+          {
+            place: "living",
+            temperature: "error",
+            humidity: "error",
+          },
+          {
+            place: "desk",
+            temperature: "error",
+            humidity: "error",
+          },
+          {
+            place: "bedroom",
+            temperature: "error",
+            humidity: "error",
+          },
+        ]);
+        setFetchTime(new Date());
+      }
+    };
+
+    // 初回のデータフェッチ
+    fetchData();
+
+    // 一定時間ごとにfetchDataを実行
+    const intervalId = setInterval(fetchData, 900000);
+
+    // クリーンアップ関数でインターバルをクリア
+    return () => clearInterval(intervalId);
+  }, []);
 
   const handleClickOpen = async (endpoint) => {
     setBdOpen(true);
@@ -98,6 +119,48 @@ const App = () => {
         Smart Home Remote
       </Typography>
       <Grid container spacing={2}>
+        {thermodata.map((item, index) => (
+          <Grid item xs={4} sm={4} md={4}>
+            <Card sx={{ minwidth: 275 }}>
+              <CardContent>
+                <Typography
+                  sx={{ fontSize: 14 }}
+                  color="text.secondary"
+                  gutterBottom
+                >
+                  {item["place"]}
+                </Typography>
+                <Typography
+                  sx={{ fontSize: 14 }}
+                  color="text.secondary"
+                  gutterBottom
+                >
+                  <span style={{ fontSize: "32px" }}>
+                    {item["temperature"]} ℃ / {item["humidity"]} ％
+                  </span>
+                </Typography>
+                <Typography
+                  sx={{ fontSize: 32 }}
+                  color="text.secondary"
+                  gutterBottom
+                >
+                  不快指数：
+                  {37 -
+                    (37 - item["temperature"]) /
+                      (0.68 - 0.0014 * item["humidity"] + 1 / 1.76) -
+                    0.29 * item["temperature"] * (1 - item["humidity"] / 100)}
+                </Typography>
+                <Typography
+                  sx={{ fontSize: 12, textAlign: "right" }}
+                  color="text.secondary"
+                  gutterBottom
+                >
+                  {fetchTime.toLocaleTimeString()} 時点
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
         {buttons.map((button, index) => (
           <Grid item xs={12} sm={6} md={6} key={index}>
             <Button
